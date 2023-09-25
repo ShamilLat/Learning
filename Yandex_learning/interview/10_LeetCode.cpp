0ae816a350a7de4ba625dc79fc62f62795315cac
Шамиль Латыпов <2Lapsha1@gmail.com>
2023-09-25 20:33:23 +0300

    start code

---
+++ Yandex_learning/interview/10_LeetCode.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+Объединить слова по анаграмам.
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+*/
+
+class Solution {
+ public:
+  vector<vector<string>> groupAnagrams(vector<string>& strs) {}
+};

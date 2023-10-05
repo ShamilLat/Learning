d699ceffbbfb2eab4ea741bada29efdc7c8a7433
Шамиль Латыпов <2Lapsha1@gmail.com>
2023-10-05 23:41:37 +0300

    13_LeetCode done

---
+++ Yandex_learning/interview/13_LeetCode.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+template <typename Type>
+ostream& operator<<(ostream& out, vector<Type> vec) {
+  for (auto& i : vec) {
+    out << i << " ";
+  }
+  return out;
+}
+
+class Solution {
+ public:
+  void rec(string str, int open, int close) {
+    if (open == 0 && close == 0) {
+      res.push_back(str);
+      return;
+    } else if (open == close) {
+      str.push_back('(');
+      rec(str, open - 1, close);
+    } else if (open == 0) {
+      str.push_back(')');
+      rec(str, open, close - 1);
+    } else {
+      str.push_back('(');
+      rec(str, open - 1, close);
+
+      str.pop_back();
+      str.push_back(')');
+      rec(str, open, close - 1);
+    }
+  }
+
+  vector<string> generateParenthesis(int n) {
+    rec("", n, n);
+    return res;
+  }
+
+  vector<string> res;
+};
+
+int main() {
+  int n;
+  cin >> n;
+
+  Solution solv;
+  auto vec = solv.generateParenthesis(n);
+  cout << vec << endl;
+
+  return 0;
+}

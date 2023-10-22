2d10303b5e32f6cb5aa2cfcac548ce62571b1309
unknown <2lapsha1@gmail.com>
2023-10-22 13:40:39 +0300

    23_LeetCode done

---
+++ Yandex_learning/interview/23_LeetCode.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int trap(vector<int>& height) {
+    stack<pair<int, int>> st;
+    int res = 0;
+    for (int i = 0; i < height.size(); i++) {
+      if (height[i] == 0) {
+        continue;
+      }
+      int prev_max = 0;
+      while (!st.empty() && st.top().first <= height[i]) {
+        res += (i - st.top().second - 1) * (st.top().first - prev_max);
+        prev_max = max(prev_max, st.top().first);
+        st.pop();
+      }
+      if (!st.empty()) {
+        res += (i - st.top().second - 1) * (height[i] - prev_max);
+      }
+      st.push({height[i], i});
+    }
+    return res;
+  }
+};
+
+int main() {
+  vector<int> vec;
+  int num;
+  while (cin >> num) {
+    vec.push_back(num);
+  }
+
+  Solution solv;
+  cout << solv.trap(vec) << endl;
+}

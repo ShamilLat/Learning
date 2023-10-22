19dd1757a57090622ec04b6c1fb695f3caf9f013
unknown <2lapsha1@gmail.com>
2023-10-22 20:33:22 +0300

    26_LeetCode change

--- Yandex_learning/interview/26_LeetCode.cpp
+++ Yandex_learning/interview/26_LeetCode.cpp
@@ -21,31 +21,36 @@ class Solution {
   vector<int> findAnagrams(string s, string p) {
     vector<int> ans;
     unordered_map<char, int> m;
-    for (auto x : p)
-      m[x]++;
+    for (auto& letter : p)
+      m[letter]++;
     int count = m.size();
     int i = 0, j = 0, k = p.size();
 
+    for (; j < p.size(); j++) {
+      if (m.contains(s[j])) {
+        m[s[j]]--;
+        if (m[s[j]] == 0)
+          count--;
+      }
+    }
+
+    j--;
     while (j < s.size()) {
       if (m.contains(s[j])) {
         m[s[j]]--;
         if (m[s[j]] == 0)
           count--;
       }
-      if (j - i + 1 < k) {
-        j++;
-      } else if (j - i + 1 == k) {
-        if (count == 0)
-          ans.push_back(i);
+      if (count == 0)
+        ans.push_back(i);
 
-        if (m.contains(s[i])) {
-          m[s[i]]++;
-          if (m[s[i]] == 1)
-            count++;
-        }
-        i++;
-        j++;
+      if (m.contains(s[i])) {
+        m[s[i]]++;
+        if (m[s[i]] == 1)
+          count++;
       }
+      i++;
+      j++;
     }
     return ans;
   }

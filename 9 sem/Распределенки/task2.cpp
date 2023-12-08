b45e5daa496eeac480b9f0aea6b1a5799c96a454
Шамиль Латыпов <2Lapsha1@gmail.com>
2023-12-08 13:47:21 +0300

    tasks

--- 9 sem/Распределенки/task2.cpp
+++ 9 sem/Распределенки/task2.cpp
@@ -1,22 +1,21 @@
 #include <limits.h>
 #include <mpi.h>
 #include <algorithm>
+#include <cfloat>
 #include <iostream>
 #include <random>
 #include <vector>
 
 void add_comp(int first, int second, int rank, std::vector<double>& data) {
   int other_rank = (rank == first) ? second : first;
-  std::vector<double> other_data;
+  std::vector<double> other_data(data.size());
 
   // Отправляем и получаем данные
   int size;
-  MPI_Sendrecv(&data[0], data.size(), MPI_DOUBLE, other_rank, 0, &size, 1,
-               MPI_INT, other_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  std::cout << "RANKS " << first << " " << second << std::endl;
 
-  other_data.resize(size);
   MPI_Sendrecv(&data[0], data.size(), MPI_DOUBLE, other_rank, 0, &other_data[0],
-               size, MPI_DOUBLE, other_rank, 0, MPI_COMM_WORLD,
+               data.size(), MPI_DOUBLE, other_rank, 0, MPI_COMM_WORLD,
                MPI_STATUS_IGNORE);
 
   // Объединение и сортировка
@@ -29,6 +28,7 @@ void add_comp(int first, int second, int rank, std::vector<double>& data) {
   } else {
     data = std::vector<double>(data.begin() + data.size() / 2, data.end());
   }
+  std::cout << "RANKS " << first << " " << second << " END" << std::endl;
 }
 
 void S(int first1,
@@ -56,18 +56,14 @@ void S(int first1,
   S(first1 + step, first2 + step, 2 * step, n - n1, m - m1, rank, vec);
 
   for (i = 1; i < n - 1; i += 2) {
-    int rank1 = first1 + step * i;
-    int rank2 = first1 + step * (i + 1);
-    if (rank == rank1 || rank == rank2) {
-      add_comp(rank1, rank2, rank, vec);
+    if (rank == first1 + step * i || rank == first1 + step * (i + 1)) {
+      add_comp(first1 + step * i, first1 + step * (i + 1), rank, vec);
     }
   }
 
   if (n % 2 == 0) {
-    int rank1 = first1 + step * (n - 1);
-    int rank2 = first2;
-    if (rank == rank1 || rank == rank2) {
-      add_comp(rank1, rank2, rank, vec);
+    if (rank == first1 + step * (n - 1) || rank == first2) {
+      add_comp(first1 + step * (n - 1), first2, rank, vec);
     }
     i = 1;
   } else {
@@ -75,10 +71,8 @@ void S(int first1,
   }
 
   for (; i < m - 1; i += 2) {
-    int rank1 = first2 + step * i;
-    int rank2 = first2 + step * (i + 1);
-    if (rank == rank1 || rank == rank2) {
-      add_comp(rank1, rank2, rank, vec);
+    if (rank == first2 + step * i || rank == first2 + step * (i + 1)) {
+      add_comp(first2 + step * i, first2 + step * (i + 1), rank, vec);
     }
   }
 }
@@ -88,10 +82,8 @@ void B(int first, int step, int count, int rank, std::vector<double>& vec) {
     return;
   }
   if (count == 2) {
-    int rank1 = first;
-    int rank2 = first + step;
-    if (rank == rank1 || rank == rank2) {
-      add_comp(rank1, rank2, rank, vec);
+    if (rank == first || rank == first + step) {
+      add_comp(first, first + step, rank, vec);
     }
     return;
   }
@@ -132,7 +124,13 @@ int main(int argc, char* argv[]) {
 
   if (array_size % size != 0) {
     int cnt = size - array_size % size;
-    data.insert(data.end(), cnt, DBL_MAX);
+    if (rank == 0) {
+      data.insert(data.end(), cnt, DBL_MAX);
+      for (auto& i : data) {
+        std::cout << i << " ";
+      }
+      std::cout << std::endl;
+    }
     array_size += cnt;
   }
 
@@ -148,7 +146,7 @@ int main(int argc, char* argv[]) {
 
   std::sort(local_data.begin(), local_data.end());
 
-  B(0, 1, local_size, rank, local_data);
+  B(0, 1, size, rank, local_data);
 
   MPI_Gather(&local_data[0], local_size, MPI_DOUBLE, &data[0], local_size,
              MPI_DOUBLE, 0, MPI_COMM_WORLD);
